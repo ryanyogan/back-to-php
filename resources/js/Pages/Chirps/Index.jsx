843298651a14fc62827466d9c16b768e9cf57bef
Ryan Yogan <ryanyogan@gmail.com>
2024-10-10 20:47:23 -0500

    getting there

---
+++ resources/js/Pages/Chirps/Index.jsx
@@ -0,0 +1,44 @@
+import Chirp from "@/Components/Chirp";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, useForm } from "@inertiajs/react";
+
+export default function Index({ chirps }) {
+    const { data, setData, post, reset, errors, processing } = useForm({
+        message: "",
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route("chirps.store"), { onSuccess: () => reset() });
+    };
+
+    return (
+        <AuthenticatedLayout>
+            <Head title="Chirps" />
+
+            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+                <form onSubmit={submit}>
+                    <textarea
+                        value={data.message}
+                        placeholder="What's on your mind?"
+                        onChange={(e) => setData("message", e.target.value)}
+                        className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                    ></textarea>
+                    <InputError message={errors.message} className="mt-2" />
+
+                    <PrimaryButton className="mt-4" disabled={processing}>
+                        Chirps
+                    </PrimaryButton>
+                </form>
+
+                <div className="bg-white mt-6 shadow-sm rounded-lg divide-y">
+                    {chirps.map((chirp) => (
+                        <Chirp key={chirp.id} chirp={chirp} />
+                    ))}
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    )
+}
